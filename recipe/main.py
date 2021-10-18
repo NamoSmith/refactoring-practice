2ac194c85e74d6e3de5d52b23f98ac2db89599f2
NamoSmith <namosmith2545@gmail.com>
2021-10-18 15:43:43 +0700

    Refactoring.

--- recipe/main.py
+++ recipe/main.py
@@ -1,5 +1,16 @@
 from recipe import Recipe
 
+
+def create_recipe(name, coffee=0, chocolate=0, sugar=0, milk=0, price=0):
+    recipe = Recipe(name)
+    recipe.coffee = coffee
+    recipe.chocolate = chocolate
+    recipe.sugar = sugar
+    recipe.milk = milk
+    recipe.price = price
+    return recipe
+
+
 if __name__ == '__main__':
     recipe1 = Recipe("Coffee with sugar")
     recipe1.coffee = 4
@@ -15,7 +26,6 @@ if __name__ == '__main__':
     recipe2.price = 40.0
     print(recipe2)
 
-
     recipe3 = Recipe("Hot Chocolate")
     recipe3.coffee = 0
     recipe3.chocolate = 3
@@ -23,4 +33,3 @@ if __name__ == '__main__':
     recipe3.milk = 4
     recipe3.price = 30.0
     print(recipe3)
-    
